658aaa4cf8ea35e66f425f8cfecd83f4c26cb0a9
kwaters12 <kellywaters@gmail.com>
2015-10-13 07:13:17 -0700

    Fix heroku

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -1,10 +1,10 @@
 class PaymentNotificationsController < ApplicationController
-  protect_from_forgery, except: [:create]
+  # protect_from_forgery, except: [:create]
 
 
-  def create
-    PaymentNotification.create!(params: params, order_number: params[:invoice], status: params[:payment_status], transaction_id: params[:txn_id])
-    render :nothing => true
-  end
+  # def create
+  #   PaymentNotification.create!(params: params, order_number: params[:invoice], status: params[:payment_status], transaction_id: params[:txn_id])
+  #   render :nothing => true
+  # end
 
 end
